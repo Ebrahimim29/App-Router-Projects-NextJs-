99d86026cd78e39dc8c7f307815573c7f7890cf0
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-08-10 07:03:51 +0330

    Static Site Generation(SSG)-Data Fetching

---
+++ app/posts/page.tsx
@@ -0,0 +1,37 @@
+import { Button, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap"
+import ListGroupItemLink from "../components/ListGroupItemLink";
+
+interface Post {
+    id: number;
+    title: string;
+}
+
+const getPostsService = async () => {
+    const res = await fetch('http://localhost:4000/posts')
+    const posts = await res.json()
+    return posts
+}
+
+const Page =  async () => {
+
+    const posts = await getPostsService()
+
+    return (
+        <Container className="d-flex justify-content-center align-items-center flex-column">
+            
+            <Button className="mt-2">
+                Back
+            </Button>
+            <Row className="mt-3 w-100">
+                <ListGroup>
+                    {posts.map((p : any) => (
+                        <ListGroupItemLink key={p.id} href={`/posts/${p.id}`} title={p.title}/>
+                    ))}
+                </ListGroup>
+            </Row>
+
+        </Container>
+    )
+}
+
+export default Page;
