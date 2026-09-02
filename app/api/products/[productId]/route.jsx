75e452084c9dc30307a880b2188714f9de2da9bb
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-09-02 07:19:58 +0330

    Route Handlers in App Router-Part 2

---
+++ app/api/products/[productId]/route.jsx
@@ -0,0 +1,18 @@
+export async function GET(request) {
+// export async function GET(request, context) {
+    
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('name')
+
+    // const {params} = context
+    // const {productId} = params
+    
+
+    const data = [
+        {id:1, title:"product1", price: 1_000_000},
+        {id:2, title:"product2", price: 2_000_000}
+    ]
+
+    return Response.json({query})
+    // return Response.json({productId})
+}
